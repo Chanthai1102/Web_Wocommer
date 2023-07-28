42782b4f60c15f37282f758a1867664d073cee94
Chanthai1102 <114852768+Chanthai1102@users.noreply.github.com>
2023-07-29 01:32:42 +0700

    Update

--- web-client-front/tailwind.config.js
+++ web-client-front/tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/component/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'robot' : ['robot']
+      },
+    },
   },
   plugins: [],
 }
-
