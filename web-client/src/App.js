f6a2c5a216540cb34251b2caa88bf09e65c7981b
Chanthai1102 <114852768+Chanthai1102@users.noreply.github.com>
2023-08-03 00:09:12 +0700

    Update Template

---
+++ web-client/src/App.js
@@ -0,0 +1,11 @@
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
