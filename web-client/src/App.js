14efd467f756621800b1b57e624b61be65ba3d15
Chanthai1102 <114852768+Chanthai1102@users.noreply.github.com>
2023-08-03 13:01:40 +0700

    Update

---
+++ web-client/src/App.js
@@ -0,0 +1,10 @@
+import {LayoutDashboard} from "./component/layout/LayoutDashboard"
+function App() {
+  return (
+    <div className="App">
+      <LayoutDashboard/>
+    </div>
+  );
+}
+
+export default App;
